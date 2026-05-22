2919f01b336ac3d2b58df51c2fa1e876b1ade9f1
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-21 22:41:33 -0400

    feat(product): create prodcut type using zod

---
+++ frontend/src/features/product/types.ts
@@ -0,0 +1,12 @@
+import z from 'zod';
+
+export const productBaseSchema = z.object({
+	id: z.uuid(),
+	name: z.string(),
+	price: z.number().nonnegative(),
+	description: z.string().optional().nullable(),
+	createdAt: z.string().optional().nullable(),
+	updatedAt: z.string().optional().nullable(),
+});
+
+export type TProduct = z.infer<typeof productBaseSchema>;
