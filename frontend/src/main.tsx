d7aa60c4c2ae956cd706f506bd13f344fbb0797b
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-11 12:44:22 -0400

    feat(store): setup redux store and wrap the app with provider and persistor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from '@store';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	</StrictMode>,
 );
---
+++ frontend/src/store/index.ts
@@ -0,0 +1,28 @@
+import storage from 'redux-persist/es/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	version: 1,
+};
+
+const rootReducer = combineReducers({});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
