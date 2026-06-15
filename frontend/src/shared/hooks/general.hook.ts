9836dcb5759257205664c4b992e52f79ae314ee6
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-14 22:40:15 -0400

    update(generic hook): add the selectData and selectStatus to the useFetch

--- frontend/src/shared/hooks/general.hook.ts
+++ frontend/src/shared/hooks/general.hook.ts
@@ -1,21 +1,34 @@
 import type { AsyncThunk } from '@reduxjs/toolkit';
 import { useEffect, useMemo } from 'react';
-import { useAppDispatch } from './redux.hook';
+import { useAppDispatch, useAppSelector } from './redux.hook';
 import type { TBaseQuery } from '../schema';
 import type { ICollectionResult } from '../types';
 import { useSearchParams } from 'react-router';
+import type { RootState } from '@/store';
 
-export const useFetchData = <T, TQuery extends TBaseQuery = TBaseQuery>({
+export const useFetch = <T, TQuery extends TBaseQuery = TBaseQuery>({
 	filter,
 	thunkAction,
+	selectData,
+	selectStatus,
 }: {
 	filter: TQuery;
 	thunkAction: AsyncThunk<ICollectionResult<T>, TQuery, { rejectValue: string }>;
+	selectData: (state: RootState) => T[];
+	selectStatus: (state: RootState) => string;
 }) => {
 	const dispatch = useAppDispatch();
 	useEffect(() => {
 		dispatch(thunkAction(filter as unknown as TQuery & undefined));
 	}, [dispatch]);
+
+	const data = useAppSelector(selectData);
+	const status = useAppSelector(selectStatus);
+
+	return {
+		data,
+		status,
+	};
 };
 
 export const useQueryParams = <TQuery extends TBaseQuery = TBaseQuery>({ schema }: { schema: z.ZodSchema<TQuery> }) => {
