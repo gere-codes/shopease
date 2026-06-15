be4cc94ecc0ca7cd6b8f5ffd03e4c28a39ff16ff
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-14 22:25:10 -0400

    feat(general hook): create  useQueryParams generic hook

--- frontend/src/shared/hooks/general.hook.ts
+++ frontend/src/shared/hooks/general.hook.ts
@@ -1,18 +1,35 @@
 import type { AsyncThunk } from '@reduxjs/toolkit';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useAppDispatch } from './redux.hook';
 import type { TBaseQuery } from '../schema';
 import type { ICollectionResult } from '../types';
+import { useSearchParams } from 'react-router';
 
 export const useFetchData = <T, TQuery extends TBaseQuery = TBaseQuery>({
 	filter,
 	thunkAction,
 }: {
-	filter: T;
+	filter: TQuery;
 	thunkAction: AsyncThunk<ICollectionResult<T>, TQuery, { rejectValue: string }>;
 }) => {
 	const dispatch = useAppDispatch();
 	useEffect(() => {
 		dispatch(thunkAction(filter as unknown as TQuery & undefined));
 	}, [dispatch]);
+};
+
+export const useQueryParams = <TQuery extends TBaseQuery = TBaseQuery>({ schema }: { schema: z.ZodSchema<TQuery> }) => {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const filters = useMemo(() => {
+		try {
+			const queryParams = Object.fromEntries(searchParams.entries());
+			return schema.parse(queryParams);
+		} catch (error) {
+			console.error(error);
+			return schema.parse({});
+		}
+	}, [searchParams, schema]);
+
+	return { filters, searchParams, setSearchParams };
 };
