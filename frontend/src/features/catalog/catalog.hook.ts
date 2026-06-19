da478e8246bcddc5a290d7eee58b020abd36141c
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-19 12:42:25 -0400

    update(catalog hook): remove the sortMap

--- frontend/src/features/catalog/catalog.hook.ts
+++ frontend/src/features/catalog/catalog.hook.ts
@@ -1,18 +1,6 @@
 import { useUrlParams } from '@/shared/hooks';
 import { productQuerySchema, type TProductQuery } from '@/shared/schema';
-import { useEffect, useState } from 'react';
-
-type SortKey = 'createdAt' | 'price-low' | 'price-high';
-type SortConfig = {
-	sort: TProductQuery['sort'];
-	order: TProductQuery['order'];
-};
-
-const sortMap: Record<SortKey, SortConfig> = {
-	createdAt: { sort: 'createdAt', order: 'desc' },
-	'price-low': { sort: 'price', order: 'asc' },
-	'price-high': { sort: 'price', order: 'desc' },
-};
+import { useState } from 'react';
 
 export const useCatalogActions = () => {
 	const { filters, setParam, setParamsDebounce, searchParams, clearAllParams } = useUrlParams<TProductQuery>({
@@ -20,36 +8,34 @@ export const useCatalogActions = () => {
 	});
 
 	const [localFilters, setLocalFilters] = useState<TProductQuery>(filters);
-	console.log('re-rendered', localFilters?.sort, localFilters?.order);
-
-	useEffect(() => {
-		setLocalFilters(filters);
-	}, [filters]);
 
 	const handleChanges = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 		const { name, value } = e.target;
 
-		const val = name === 'maxPrice' ? Number(value) : value;
+		const val = name === 'maxPrice' || name === 'minPrice' ? Number(value) : value;
 
 		setLocalFilters((prev) => ({
 			...prev,
 			[name]: val,
 		}));
 
-		if (name === 'maxPrice') {
-			setParamsDebounce({ [name]: val });
-		} else if (name === 'sort') {
-			const sort = sortMap[value as SortKey];
-			setParam(sort);
+		if (name === 'maxPrice' || name === 'minPrice') {
+			setParamsDebounce<TProductQuery>({ [name]: Number(val) });
 		} else {
 			setParam({ [name]: val });
 		}
 	};
 
 	return {
-		handleChanges,
+		// Data
 		localFilters,
-		clearAllFilters: clearAllParams,
 		searchParams,
+
+		// Actions
+		handleChanges,
+		clearAllFilters: () => {
+			clearAllParams();
+			setLocalFilters({ maxPrice: 300, category: 'all' } as TProductQuery);
+		},
 	};
 };
