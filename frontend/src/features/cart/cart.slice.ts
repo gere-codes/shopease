e0a2d7bf7ea95fc1ab6c539799cae8d314e2e67d
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-19 19:53:06 -0400

    feat(cart slice): add the increment logic

--- frontend/src/features/cart/cart.slice.ts
+++ frontend/src/features/cart/cart.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { TCart } from './cart.schema';
 
 interface CartState {
@@ -11,5 +11,19 @@ const initialState: CartState = {
 export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
-	reducers: {},
+	reducers: {
+		increment: (state, action: PayloadAction<TCart>) => {
+			const payload = action.payload;
+
+			const itemIndex = state.cart.findIndex((item) => item.product.id === payload.product.id);
+			if (itemIndex !== -1) {
+				const item = state.cart[itemIndex];
+				if (item.quantity < item.product.quantity) {
+					item.quantity += 1;
+				}
+			} else {
+				state.cart.push({ ...payload, quantity: 1 });
+			}
+		},
+	},
 });
