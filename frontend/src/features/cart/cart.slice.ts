565fa33133bac357d6f2d5d4faab4756c7bc0f56
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-19 20:02:14 -0400

    feat(cart slice): implement decrement logic

--- frontend/src/features/cart/cart.slice.ts
+++ frontend/src/features/cart/cart.slice.ts
@@ -25,5 +25,19 @@ export const cartSlice = createSlice({
 				state.cart.push({ ...payload, quantity: 1 });
 			}
 		},
+
+		decrement: (state, action) => {
+			const payload = action.payload;
+
+			const itemIndex = state.cart.findIndex((item) => item.product.id === payload.product.id);
+			if (itemIndex !== -1) {
+				const item = state.cart[itemIndex];
+				if (item.quantity === 1) {
+					state.cart.splice(itemIndex, 1);
+				} else {
+					item.quantity--;
+				}
+			}
+		},
 	},
 });
