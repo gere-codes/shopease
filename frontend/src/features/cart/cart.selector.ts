054082aede5509f28182646667f923463236dd86
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-20 23:51:18 -0400

    feat(cart selector): create selectTotalCosts selector and calculate the sub total, shipping, tax and total cost

--- frontend/src/features/cart/cart.selector.ts
+++ frontend/src/features/cart/cart.selector.ts
@@ -3,6 +3,21 @@ import type { RootState } from '@/store';
 export const selectCartItems = (state: RootState) => state.cart.items;
 export const selectCartTotalItems = (state: RootState) =>
 	state.cart.items.reduce((total, item) => total + item.quantity, 0);
+
+export const selectTotalCost = (state: RootState) => {
+	const subtotal = state.cart.items.reduce((total, item) => total + item.quantity * item.product.price, 0);
+	const shippingEst = subtotal > 150 || subtotal === 0 ? 0 : 9.99;
+	const taxRate = 0.13;
+	const taxEst = taxRate * subtotal;
+	const totalCost = subtotal + shippingEst + taxEst;
+
+	return {
+		subtotal,
+		shippingEst,
+		totalCost,
+		taxEst,
+	};
+};
 export const selectCartItemQuantity = (state: RootState, id: string) => {
 	const item = state.cart.items.find((i) => i.product.id === id);
 	return item ? item.quantity : 0;
