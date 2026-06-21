0abf439aacdff978a7cf55ba411f8975fcac1c37
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-20 20:26:07 -0400

    feat(product page): dispatch the add cart action when the add button is clicked

--- frontend/src/pages/product/product.page.tsx
+++ frontend/src/pages/product/product.page.tsx
@@ -5,6 +5,8 @@ import { useAppDispatch, useAppSelector } from '@/shared/hooks/redux.hook';
 import { selectProductItemData } from '@/shared/store/product/product.selector';
 import { productThunks } from '@/shared/store/product/product.thunks';
 import { BASE_URL } from '@/shared/api';
+import { cartAction, type TCart } from '@/features/cart';
+import type { TProduct } from '@/shared/schema';
 
 export const ProductPage = memo(() => {
 	const { id } = useParams();
@@ -19,14 +21,22 @@ export const ProductPage = memo(() => {
 	if (!product) {
 		return <h3>loading...</h3>;
 	}
+
+	const handleAddToCart = (item: TProduct) => {
+		const newCart: TCart = {
+			product: item,
+			quantity: 1,
+		};
+		dispatch(cartAction.add(newCart));
+	};
 	return (
 		<Container>
 			{/* Main layout container */}
-			<section className="flex flex-col gap-6 md:flex-row items-start py-6">
+			<section className="flex flex-col gap-8 md:flex-row items-start py-6 max-w-5xl mx-auto">
 				{/* Left: Product images*/}
 				<ProductImages product={product} baseUrl={BASE_URL} />
 				{/* Right: product details */}
-				<ProductDetails product={product} />
+				<ProductDetails product={product} add={handleAddToCart} />
 			</section>
 		</Container>
 	);
