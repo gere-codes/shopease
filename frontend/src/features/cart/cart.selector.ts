aaae9c5803b261764d8860d9dfff9510d1ef844c
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-20 20:43:24 -0400

    feat(cart): create a selector that return the total number of items

--- frontend/src/features/cart/cart.selector.ts
+++ frontend/src/features/cart/cart.selector.ts
@@ -1,6 +1,8 @@
 import type { RootState } from '@/store';
 
 export const selectCartItems = (state: RootState) => state.cart.items;
+export const selectCartTotalItems = (state: RootState) =>
+	state.cart.items.reduce((total, item) => total + item.quantity, 0);
 export const selectCartItemQuantity = (state: RootState, id: string) => {
 	const item = state.cart.items.find((i) => i.product.id === id);
 	return item ? item.quantity : 0;
