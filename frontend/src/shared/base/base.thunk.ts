57d0741ade6043d75b727f0c71ad88dffd4fa2db
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-24 12:45:29 -0400

    update(base thunk): add the TKey param

--- frontend/src/shared/base/base.thunk.ts
+++ frontend/src/shared/base/base.thunk.ts
@@ -4,8 +4,12 @@ import type { IBaseService } from './base.service';
 import type { ICollectionResult } from '../types';
 import { isAxiosError } from 'axios';
 
-export interface BaseThunksResult<T, TQuery> {
-	getCollection: AsyncThunk<ICollectionResult<T>, TQuery, { rejectValue: string }>;
+export interface BaseThunksResult<T, TKey extends string, TQuery> {
+	getCollection: AsyncThunk<
+		{ collection: ICollectionResult<T>; key: TKey },
+		{ filters: TQuery; key: TKey },
+		{ rejectValue: string }
+	>;
 	getById: AsyncThunk<T, string, { rejectValue: string }>;
 }
 
@@ -19,22 +23,28 @@ export const handleThunkError = (error: unknown, defaultMessage: string): string
 
 export const createBaseThunks = <
 	T,
+	TKey extends string,
 	TQuery extends TBaseQuery,
 	TService extends IBaseService<T, TQuery> = IBaseService<T, TQuery>,
 >(
 	resource: string,
 	service: TService,
-): BaseThunksResult<T, TQuery> => {
-	const getCollection = createAsyncThunk<ICollectionResult<T>, TQuery, { rejectValue: string }>(
-		`${resource}/getCollection`,
-		async (params, { rejectWithValue }) => {
-			try {
-				return await service.getCollection(params);
-			} catch (error) {
-				return rejectWithValue(handleThunkError(error, `Error fetching ${resource}s`));
-			}
-		},
-	);
+): BaseThunksResult<T, TKey, TQuery> => {
+	const getCollection = createAsyncThunk<
+		{ collection: ICollectionResult<T>; key: TKey },
+		{ filters: TQuery; key: TKey },
+		{ rejectValue: string }
+	>(`${resource}/getCollection`, async ({ filters, key }, { rejectWithValue }) => {
+		try {
+			const collection = await service.getCollection(filters);
+			return {
+				collection: collection,
+				key: key,
+			};
+		} catch (error) {
+			return rejectWithValue(handleThunkError(error, `Error fetching ${resource}s`));
+		}
+	});
 	const getById = createAsyncThunk<T, string, { rejectValue: string }>(
 		`${resource}/getById`,
 		async (id, { rejectWithValue }) => {
