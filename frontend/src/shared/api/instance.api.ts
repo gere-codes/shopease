5803e55728eb4e0d93ff35b15d17c5a8f286bd78
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-07-01 15:13:16 -0400

    update(api): change the development base url to match the backend

--- frontend/src/shared/api/instance.api.ts
+++ frontend/src/shared/api/instance.api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000';
+export const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000/api';
 
 // public instance
 export const publicInstance = axios.create({
-	baseURL: `${BASE_URL}/api`,
+	baseURL: `${BASE_URL}`,
 	withCredentials: true,
 });
