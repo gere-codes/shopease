a18edef4c12ccac34a78eca90aa89a684ec8577c
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-07-08 14:21:57 -0400

    update(category: delete the category carousel component

--- frontend/src/shared/components/common/index.ts
+++ frontend/src/shared/components/common/index.ts
@@ -4,7 +4,6 @@ export { PromoBanner } from './banner.common';
 export { ProductSection } from './product/product.section';
 export { ProductGrid } from './product/product.grid';
 export { ProductItem } from './product/product.item';
-export { CategoryCarousel } from './category/category.carousel';
 export { Container } from './container.common';
 export { ProductDetails } from './product/product.details';
 export { ProductImages } from './product/product.images';
